8d2d32a33c5a68ee4c468a6712cc37d67440d58d
AlaynF <alayn@iMac.local>
2016-04-02 15:49:59 -0400

    Finish DB

--- scrape.js
+++ scrape.js
@@ -1,6 +1,21 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var games = [];
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'livegames'
+})
+
+  connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
+      connection.query('INSERT INTO livegames (home, away, a, name) VALUES (home, away, a, name )', ['home', 'away', 'a','name'], function(err, result) {
+      if (err) throw err
+  })
+})
 
 request('http://www.stream2watch.co/live-now', function (error, response, html) {
   if (!error && response.statusCode == 200) {
