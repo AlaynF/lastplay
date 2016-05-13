3b998d332d20ac13d1f6976fe714615dae4512a2
Alayn Fernandez <Alayn@Alayns-MacBook-Pro.local>
2016-05-12 22:29:06 -0400

    player

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,7 @@ window.app.controller('game', function($scope, $http, $location, $window) {
 
     window.onload = function(){
         var player = new Clappr.Player({
-            source: "https://manifest.googlevideo.com/api/manifest/hls_variant/maudio/1/requiressl/yes/ratebypass/yes/ip/172.98.67.50/key/yt6/ipbits/0/go/1/fexp/11200715%2C9416891%2C9422596%2C9428269%2C9428398%2C9428492%2C9430058%2C9431012%2C9433096%2C9433946%2C9435173%2C9436446%2C9436499/hfr/1/sparams/demuxed%2Cgcr%2Cgo%2Chfr%2Cid%2Cip%2Cipbits%2Citag%2Cmaudio%2Cplaylist_type%2Cratebypass%2Crequiressl%2Csource%2Cexpire/upn/zJp5w1NOoCs/id/MYdPVuUX874.1/itag/0/signature/D0BD5DC7D08522A3007B187A816DF71B9242D397.A066C2400BA6A9A7F0813A04D439C8F25041FEEA/gcr/ca/sver/3/expire/1463029416/demuxed/1/keepalive/yes/playlist_type/LIVE/source/yt_live_broadcast/file/index.m3u8",
-            parentId: "#player",
+            source: "https://manifest.googlevideo.com/api/manifest/hls_variant/expire/1463119739/go/1/id/gufohV_VMQY.1/sver/3/maudio/1/playlist_type/LIVE/fexp/9416891%2C9422596%2C9428269%2C9428398%2C9428492%2C9430058%2C9431012%2C9433096%2C9433946%2C9435173%2C9436446%2C9436499/ipbits/0/demuxed/1/sparams/demuxed%2Cgo%2Chfr%2Cid%2Cip%2Cipbits%2Citag%2Cmaudio%2Cplaylist_type%2Cratebypass%2Crequiressl%2Csource%2Cexpire/source/yt_live_broadcast/itag/0/ratebypass/yes/keepalive/yes/upn/TCGTP2dsH8g/requiressl/yes/key/yt6/ip/172.98.67.79/hfr/1/signature/CEF8A0E0ED52BF2806221058FE54F1E14A9ECC85.155BAEC7CD42B3BB4B0442EF8FEA6E2673B2583E/file/index.m3u8",
             poster: 'http://cors.io/?u=http://wallpaperhd.press/wp-content/uploads/2016/02/Sports-Wallpapers-L3O.jpg',
             width: "960px",
             height: "400px"
