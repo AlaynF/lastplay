cb4281459b51c9c6f7223b803ebe93326b73d8a2
Alayn Fernandez <Alayn@Alayns-MacBook-Pro.local>
2016-05-20 19:27:02 -0400

    live

--- assets/js/home.js
+++ assets/js/home.js
@@ -58,39 +58,19 @@ window.app.controller('home', function($scope, $http, $location, $window, $filte
 
     window.onload = function(){
         var player = new Clappr.Player({
-            source: "http://localhost:1337/api/reddit/proxy?url=http://video-edge-836134.sfo01.hls.ttvnw.net/hls30/trescojones_21388201920_455075759/chunked/index-live.m3u8?token=id=614553183915019484,bid=21388201920,exp=1463687406,node=video-edge-836134-1.sfo01.hls.justin.tv,nname=video-edge-836134.sfo01,fmt=chunked&sig=e12fb9eb063be42fb49f2dfbd100420fee68d8b2&wow=.m3u8",
-            poster: 'http://athlonsports.com/sites/athlonsports.com/files/EasternConfFinals_0.jpg',
-            width: "960px",
-            parentId: "#player",
-            height: "400px"
+        source: "rtmp://localonly.chickenkiller.com:1935/live/test",
+        parentId: "#player-wrapper",
+        plugins: {'playback': [RTMP]},
+        rtmpConfig: {},
         });
 
-        $scope.logout = function () {
-            console.log('her');
-            session.userId = null;
-            res.redirect('/login')
-        };
-
-
-        // function playM3u8(url){
-        //     console.log(Hls.isSupported());
-        //   if(Hls.isSupported()) {
-        //       console.log('ok');
-        //       var video = document.getElementById('video');
-        //       var hls = new Hls();
-        //       var m3u8Url = decodeURIComponent(url)
-        //       console.log(hls.loadSource);
-        //       hls.loadSource(m3u8Url);
-        //       hls.attachMedia(video);
-        //       hls.on(Hls.Events.MANIFEST_PARSED,function() {
-        //         video.play();
-        //       });
-        //       document.title = url
-        //     }
-        // }
-        //
-        // playM3u8("http://localhost:1337/api/reddit/proxy?url=http://video-edge-836134.sfo01.hls.ttvnw.net/hls30/trescojones_21388201920_455075759/chunked/index-live.m3u8?token=id=614553183915019484,bid=21388201920,exp=1463687406,node=video-edge-836134-1.sfo01.hls.justin.tv,nname=video-edge-836134.sfo01,fmt=chunked&sig=e12fb9eb063be42fb49f2dfbd100420fee68d8b2");
     }
+
+    $scope.logout = function () {
+        console.log('her');
+        session.userId = null;
+        res.redirect('/login')
+    };
 
     // $scope.empty = function () {
     //     if(){
